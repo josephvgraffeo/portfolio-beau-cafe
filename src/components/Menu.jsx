9256ac5154de79763bcd31d1b76fa8385b6eb9c7
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-03 11:41:52 -0500

    added styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,34 @@
+import {useState} from "react";
+import "./menu.css"
+
 export default function Menu() {
+    const [coffees, setCoffees] = useState();
+    const [message, setMessage] = useState("Welcome to the Beau Café!");
+    const getCoffees = (temperature) => {
+      setMessage("Loading...")
+      fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+      .then(res => res.json())
+      .then(setCoffees)
+      .catch(alert)
+    }
+    
     return (
     <article>
-      <h2>Welcome to the Beau Café</h2>
+      <div>
+        <button onClick={() => getCoffees("hot")}>Hot Coffees</button>
+        <button onClick={() => getCoffees("iced")}>Iced Coffees</button>
+      </div>
+      {!coffees
+      ? <h2>{message}</h2>
+      : <section className="coffee-container">
+          {coffees.map(coffee => (
+          <div key={coffee.id} className="coffee-card">
+            <img src={coffee.image} alt={coffee.title} />
+            <h3>{coffee.title}</h3>
+            <p>{coffee.description}</p>
+          </div>))}
+      </section>
+      };
     </article>
     );
 }
